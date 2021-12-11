ec277ee61ed86ad5000a52cc9b95fa54b0bcd25e
Nevar <gienek.eu.pl@gmail.com>
2021-12-11 19:03:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
-const app = express()
 const path = require('path') 
-app.get('/Exploit.class', function (req, res) {
-	res.sendFile("Exploit.class", {root: path.join(__dirname)}, function (err) {
-		if (err) {console.log(err)} 
-		else {console.log("Sent file!")} 
-	})
-})
-
-		     
-app.listen(80) 
+const PORT = process.env.PORT || 5000
+express()
+	.set('exploit', path.join(__dirname, 'Exploit.class'))
+	.set('view engine', 'ejs')
+	.get('/Exploit.class', function (req, res) {
+		res.sendFile("Exploit.class", {root: path.join(__dirname)}, function (err) {
+			if (err) {console.log(err)} 
+			else {console.log("Sent file!")} 
+		})
+	})	     
+	.listen(PORT, () => console.log('listening on '+ PORT)) 
 
